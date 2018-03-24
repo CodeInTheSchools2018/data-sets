287ebf8c276f57787465060779d1278597ed6389
Kate Bladow <kbladow@gmail.com>
2018-03-23 22:06:59 -0400

    Finish parser so that it streams to disk and add document.

--- staq/ruby/311-parser/parse.rb
+++ staq/ruby/311-parser/parse.rb
@@ -1,22 +1,35 @@
-# https://dev.socrata.com/foundry/data.baltimorecity.gov/9agw-sxsr
-
 require "typhoeus"
-require "csv"
+require "pathname"
+require "pry"
 
-#request_url = "https://data.baltimorecity.gov/resource/9agw-sxsr.csv"
 request_url = "https://data.baltimorecity.gov/resource/2ddy-2uzt.csv"
 api_token = "qq0tU1zDfSVSOkTotw3ckcfHK"
+file_name = "311-data.csv"
 
-headers = { "X-App-Token": api_token }
-params = { "$limit": 600000, "$where": "violdate between '2017-01-01T00:00:00' and '2017-12-31T23:59:59'" }
-response = Typhoeus.get request_url,headers: headers,params: params
-
-options = {
-  header_converters: :symbol,
-  headers: true,
-  skip_blanks: true
+headers = {
+  "X-App-Token": api_token
 }
 
-CSV.parse(response.body,options) do |row|
-  puts row.to_hash
+params = {
+  "$limit": 600000,
+  "$where": "violdate between '2017-01-01T00:00:00' and '2017-12-31T23:59:59'"
+}
+
+request_options = {
+  method: :get,
+  followlocation: true,
+  headers: headers,
+  params: params
+}
+
+report_path = Pathname.new(file_name)
+request = Typhoeus::Request.new(request_url,request_options)
+
+report_path.open("wb") do |downloaded_file|
+  request.on_body do |chunk|
+    downloaded_file.write(chunk)
+  end
+
+  request.run
 end
+
